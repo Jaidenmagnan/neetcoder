03629acd0c50833a1576d1f2a64833e9bbef46d7
Jaiden Magnana <jaidenmagnan@gmail.com>
2025-05-18 22:05:03 -0400

    refactoring

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const { token } = require('./config.json');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages
-    ]
+        GatewayIntentBits.GuildMessages,
+    ],
 });
 
 function loadCommands() {
@@ -50,28 +50,6 @@ function loadEvents() {
     }
 }
 
-// message listener for reloading
-client.on(Events.MessageCreate, async message => {
-    console.log("message received");
-
-    if (message.content == "<@1373490238277550202> reload") {
-        // check message author
-        if (message.author == "314903883874828288" || message.author == "530872774986694656") {
-            console.log("reloading commands");
-            if (message.author == "314903883874828288") {
-                message.reply("Jaiden ur fucking weird");
-            }
-            else {
-                message.reply("wsg gang")
-            }
-            loadCommands();
-        }
-        else {
-            console.log("incorrect user");
-            message.reply("You can't run this command");
-        }
-    }
-});
 
 loadCommands();
 loadEvents();
