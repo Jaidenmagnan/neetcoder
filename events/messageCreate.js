69efb7b807fa1c5974d0014ab73974e9e8973c9b
Jaiden Magnana <jaidenmagnan@gmail.com>
2025-05-20 22:31:56 -0400

    level card

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -33,18 +33,20 @@ module.exports = {
                 const madLibsStory = generateMadLibs(message.author.id);
                 if (madLibsStory && madLibsStory.trim() !== '') {
                     await message.channel.send(madLibsStory);
-                } else {
+                }
+                else {
                     console.error('Empty story generated');
                     await message.channel.send("Oops! The story generator had a brain fart. Please try again!");
                 }
-            } catch (error) {
+            }
+            catch (error) {
                 console.error('Error in message handler:', error);
                 await message.channel.send("Oops! Something went wrong. Please try again!");
             }
         }
 
-        else if (message.content.trim() == "<@1373490238277550202>"){
-            if(message.author == "736025260800868423") {
+        else if (message.content.trim() == "<@1373490238277550202>") {
+            if (message.author == "736025260800868423") {
                 await message.reply("Can u leave me alone ur actually weird...");
             }
             else {
@@ -53,13 +55,13 @@ module.exports = {
         }
 
         else {
-            let r = Math.floor(Math.random() * 20);
-            if(r == 0) {
+            let r = Math.floor(Math.random() * 200);
+            if (r == 0) {
                 await message.reply("Hi!");
             }
-            else if(message.author == "314903883874828288" || message.author == "210133839861907456") {
-                r = Math.floor(Math.random() * 5);
-                if(r == 0) {
+            else if (message.author == "314903883874828288" || message.author == "210133839861907456") {
+                r = Math.floor(Math.random() * 500);
+                if (r == 0) {
                     await message.reply("ur fat :joy:");
                     await message.reply("https://tenor.com/view/hulk-smash-gif-12677792749566644516");
                 }
@@ -75,26 +77,6 @@ module.exports = {
     },
 };
 
-function step(level, message_count) {
-    points = [
-        [1, 5],
-        [2, 25],
-        [3, 50],
-        [4, 50],
-    ]
-    if (level <= 4) {
-        for (let i = 0; i < points.length - 1; i++) {
-            if (message_count >= (points[i][1])) {
-                level = points[i][0];
-            }
-        }
-    }
-    else {
-        level = 4 + (((message_count) - 130) % 100);
-    }
-    return level;
-
-}
 async function logXp(message) {
     let user = await Users.findOne({ where: { userid: message.author.id } });
 
@@ -105,6 +87,35 @@ async function logXp(message) {
             level: 1,
         });
     }
-
     user.increment('message_count');
+
+    user_level = user.get('level');
+    user_message_count = user.get('message_count');
+
+    new_level = calculateLevel(user_message_count);
+
+
+    if (new_level > user_level) {
+        await Users.update({ level: new_level }, { where: { userid: message.author.id } });
+
+        const userMention = `<@${message.author.id}>`;
+        message.channel.send(`${userMention} you leveled up!`);
+
+    }
 };
+
+function calculateLevel(message_count) {
+    if (message_count < 10) {
+        return 1;
+    }
+
+    if (message_count < 35) {
+        return 2;
+    }
+
+    if (message_count < 85) {
+        return 3;
+    }
+
+    return 4 + Math.floor((message_count - 85) / 100);
+}
