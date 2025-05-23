76c3ebf7e7770d2aee479888d567ed704dc2a69f
Jaiden Magnan <jaidenmagnan@gmail.com>
2025-05-23 00:39:48 -0400

    heroku stuff

--- models.js
+++ models.js
@@ -1,15 +1,31 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-    logging: false,
-});
+const sequelize = process.env.DATABASE_URL 
+  ? // production db
+    new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : // local db
+    new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS, 
+      {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        logging: false,
+      },
+    );
 
 // this is how we make a database table
 const Users = sequelize.define('users', {
