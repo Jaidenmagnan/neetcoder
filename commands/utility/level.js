92d54ef6cffcd15b2206ee026923a7303d7d3482
Jaiden Magnan <jaidenmagnan@gmail.com>
2025-06-01 17:51:21 -0400

    level fix

--- commands/utility/level.js
+++ commands/utility/level.js
@@ -1,36 +1,61 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { Users } = require('../../models.js');
-const { EmbedBuilder } = require('discord.js');
-
+const { SlashCommandBuilder } = require("discord.js");
+const { Users } = require("../../models.js");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('level')
-        .setDescription('check the level of your user'),
-    async execute(interaction) {
-        await interaction.deferReply();
-        let user = await Users.findOne({
-            where: {
-                userid: interaction.user.id,
-                guildid: interaction.guild.id,
-            },
-        });
+  data: new SlashCommandBuilder()
+    .setName("level")
+    .setDescription("check the level of your user")
 
-        if (!user) {
-            user = await Users.create({
-                userid: interaction.user.id,
-                guildid: interaction.guild.id,
-                message_count: 0,
-                level: 1,
-            });
-        }
+    .addUserOption((option) =>
+      option
+        .setName("username")
+        .setDescription("whoevers level you are checking")
+        .setRequired(false),
+    ),
+  async execute(interaction) {
+    const user_to_find = interaction.options.getUser("username");
+    await interaction.deferReply();
 
-        const level_card = new EmbedBuilder()
-            .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
-            .setDescription(`** AURA LEVEL ${await user.get('level')}**`)
-            .setTimestamp();
+    let user = null;
+    let userid = null;
+    if (user_to_find) {
+      user = await Users.findOne({
+        where: {
+          userid: user_to_find.id,
+          guildid: interaction.guild.id,
+        },
+      });
+      userid = user_to_find.id;
+    }
+ else {
+      user = await Users.findOne({
+        where: {
+          userid: interaction.user.id,
+          guildid: interaction.guild.id,
+        },
+      });
+      userid = interaction.user.id;
+    }
 
-        await interaction.editReply({ embeds: [level_card] });
+    if (!user) {
+      user = await Users.create({
+        userid: userid,
+        guildid: interaction.guild.id,
+        message_count: 0,
+        level: 1,
+      });
+    }
 
-    },
+    const name = await interaction.client.users.fetch(userid);
+    const level_card = new EmbedBuilder()
+      .setAuthor({
+        name:   name.displayName,
+        iconURL: name.displayAvatarURL(),
+      })
+      .setDescription(`** AURA LEVEL ${await user.get("level")}**`)
+      .setTimestamp();
+
+    await interaction.editReply({ embeds: [level_card] });
+  },
 };
