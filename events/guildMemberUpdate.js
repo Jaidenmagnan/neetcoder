f5d3766de24ee32593e57647642b6e441da4fbe5
Jaiden Magnan <jaidenmagnan@gmail.com>
2025-06-15 14:35:17 -0400

    start of moderation log, tracks role add and removes

---
+++ events/guildMemberUpdate.js
@@ -0,0 +1,21 @@
+const { Events } = require("discord.js");
+const { logRoleUpdate } = require("../events/ModerationLog")
+
+module.exports = {
+  name: Events.GuildMemberUpdate,
+  async execute(oldMember, newMember) {
+    const guild = oldMember.guild;
+    await guild.roles.fetch();
+    guild.roles.cache.forEach(role => {
+      if (oldMember.roles.cache.has(role.id) && !newMember.roles.cache.has(role.id)) {
+        logRoleUpdate(oldMember, newMember, role.id, false)
+      }
+
+      if (!oldMember.roles.cache.has(role.id) && newMember.roles.cache.has(role.id)) {
+        logRoleUpdate(oldMember, newMember, role.id, true)
+      }
+    })
+
+
+  },
+};
